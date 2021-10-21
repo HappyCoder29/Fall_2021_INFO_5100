f1ced2a0fdac430b49b3a06251576f10ef0520d0
Ashish <ashish@drillmaps.com>
2021-10-21 11:27:23 -0700

    Checking in changes for explaination of singleton

--- DesignPatterns/SingletonExample/src/edu/northeastern/ashish/Main.java
+++ DesignPatterns/SingletonExample/src/edu/northeastern/ashish/Main.java
@@ -3,7 +3,6 @@ package edu.northeastern.ashish;
 public class Main {
 
     public static void main(String[] args) {
-
         Projector proj = Projector.getInstance();
         Projector proj1 = Projector.getInstance();
         proj.name = "Projector in room Whitbey";
--- DesignPatterns/SingletonExample/src/edu/northeastern/ashish/Projector.java
+++ DesignPatterns/SingletonExample/src/edu/northeastern/ashish/Projector.java
@@ -2,6 +2,15 @@ package edu.northeastern.ashish;
 
 public class Projector {
 
+    /*
+    * 1. Create a class
+    * 2. Make the default constructor private this way people cannot create a new instance by saying Class c = new Class();
+    * 3. create a private static instance in the class  "private static Projector _instance;" this _instance is the one which will be treturned
+    * 4. Create a getter for this instance
+    * 5. lock it inside a synchronized so that only one person can create the instance.
+    * */
+
+
     private static Projector _instance;
     private Projector(){} // No one can create an instance through default constructor
 

c3424e6884341bf6f2943c99902f5365c8399017
Ashish <ashish@drillmaps.com>
2021-11-16 20:13:55 -0800

    Checking in code for Link List

--- LinkListExample/src/edu/northeastern/ashish/LinkList.java
+++ LinkListExample/src/edu/northeastern/ashish/LinkList.java
@@ -7,7 +7,7 @@ public class LinkList <T> {
         head = null;
     }
 
-    public void add(T data){
+    public void addToHead(T data){
         Node node = new Node(data);
 
         if(head == null){
@@ -17,8 +17,24 @@ public class LinkList <T> {
 
         node.next = head;
         head = node;
+    }
+
+
+    public void addToTail(T data){
+        Node node = new Node(data);
+        if(head == null){
+            head = node;
+            return;
+        }
+        Node<T> temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = node;
 
     }
+
+
 
     public void printList(){
         Node temp = head;
@@ -27,7 +43,240 @@ public class LinkList <T> {
             System.out.print(temp.data + " -> ");
             temp = temp.next;
         }
-        System.out.println();
+        System.out.println("NULL");
+    }
+
+    // assuming no cycle
+    public int length(){
+        Node<T> temp = head;
+        int count = 1;
+        while(temp != null ){
+            temp = temp.next;
+            count++;
+        }
+        return count;
 
     }
+
+    public void printList(Node<T> node){
+        Node temp = node;
+
+        while(temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+    public  void reverseList(){
+
+        if(head == null || head.next == null){
+            return;
+        }
+        Node<T> back = null;
+        Node<T> mid = head;
+        Node<T> front = head.next;
+
+        while(front != null){
+            mid.next = back;
+            back = mid;
+            mid = front;
+            front = front.next;
+        }
+        mid.next = back;
+        head = mid;
+    }
+
+
+    public  Node<T> reverseList(Node node){
+
+        if(node == null || node.next == null){
+            return node;
+        }
+        Node<T> back = null;
+        Node<T> mid = node;
+        Node<T> front = node.next;
+
+        while(front != null){
+            mid.next = back;
+            back = mid;
+            mid = front;
+            front = front.next;
+        }
+        mid.next = back;
+        node = mid;
+        return node;
+    }
+
+    public boolean containsCycle(){
+        if(head == null || head.next == null){
+            return false;
+        }
+        Node<T> back = head;
+        Node<T> front = head;
+
+        while(front != null){
+            front = front.next;
+            if(front == null){
+                return false;
+            }
+            front = front.next;
+            back = back.next;
+
+            if(front == back){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+
+    public Node<T> findStartOfCycle(){
+        if(head == null || head.next == null){
+            return null;
+        }
+        Node<T> back = head;
+        Node<T> front = head;
+
+        while(front != null){
+            front = front.next;
+            if(front == null){
+                return null;
+            }
+            front = front.next;
+            back = back.next;
+
+            if(front == back){
+                break;
+            }
+        }
+        if(front == null){
+            return null;
+        }
+        // At this point we know there is a cycle
+
+        front = head;
+        while(front != back){
+            front = front.next;
+            back = back.next;
+        }
+        return front;
+    }
+
+    public Node<T> getNthFromEnd(int n){
+        if(head == null || n <= 0){
+            return null;
+        }
+        Node<T> back = head;
+        Node<T> front = head;
+
+        for(int i = 0 ; i < n; i ++){
+            front = front.next;
+            if(front == null){
+                return null;
+            }
+        }
+        while(front != null){
+            front = front.next;
+            back = back.next;
+        }
+        return back;
+    }
+
+    public void deleteNthFromEnd(int n){
+        if(head == null || n <= 0){
+            return;
+        }
+        Node<T> back = head;
+        Node<T> front = head;
+
+        for(int i = 0 ; i < n+1 ; i ++){
+            front = front.next;
+            if(front == null){
+                return;
+            }
+        }
+        while(front != null){
+            front = front.next;
+            back = back.next;
+        }
+        // Back is at N+1 from end
+        Node<T> temp = back.next;
+        back.next = back.next.next;
+        temp = null;
+    }
+
+    public Node<T> breakListInHalf(){
+        if(head == null || head.next == null){
+            return null;
+        }
+        Node<T> back = head;
+        Node<T> front = head;
+
+        // assuming there is no cycle
+        while(front != null && front.next != null){
+            front = front.next.next;
+            if(front == null){break;}
+            back = back.next;
+        }
+        Node secondHalf = back.next;
+        back.next = null;
+        return secondHalf;
+
+    }
+
+    public boolean isPalindrome(){
+        if(head == null || head.next == null){
+            return true;
+        }
+        Node<T> secondHalf = breakListInHalf();
+        secondHalf = reverseList(secondHalf);
+        Node<T> temp1 = head;
+        Node<T> temp2 = secondHalf;
+        boolean bPalindrome = true;
+        while(temp1 != null && temp2 != null){
+            if(temp1.data != temp2.data){
+                bPalindrome = false;
+                break;
+            }
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        // put the list back together
+        secondHalf = reverseList(secondHalf);
+        temp1 = head;
+        while(temp1.next != null){
+            temp1 = temp1.next;
+        }
+        temp1.next = secondHalf;
+        return bPalindrome;
+
+    }
+
+    public void swapNodesInPair(){
+        if(head == null || head.next == null){
+            return;
+        }
+        Node<T> back = head;
+        Node<T> front = head.next;
+
+        while(front!= null && front.next != null){
+            T data = back.data;
+            back.data = front.data;
+            front.data = data;
+
+            front = front.next;
+            if(front == null){
+                break;
+            }
+            front = front.next;
+            back = back.next.next;
+        }
+
+
+    }
+
+
+
+
 }
--- LinkListExample/src/edu/northeastern/ashish/Main.java
+++ LinkListExample/src/edu/northeastern/ashish/Main.java
@@ -5,27 +5,92 @@ public class Main {
     public static void main(String[] args) {
 
         LinkList<Integer> list = new LinkList<>();
+        list.addToTail(1);
+        list.addToTail(2);
+        list.addToTail(3);
+        list.addToTail(4);
+        list.addToTail(5);
+        list.addToTail(6);
+        list.addToTail(7);
 
-        list.add(5);
-        list.add(-1);
-        list.add(3);
-        list.add(4);
-        list.add(8);
-
+        list.swapNodesInPair();
         list.printList();
 
-        LinkList<String> listNames = new LinkList<>();
 
-        listNames.add("Ashish");
-        listNames.add("Tom");
-        listNames.add("Peter");
-        listNames.add("Bill");
-        listNames.add("Glenn");
 
-        listNames.printList();
+        //  System.out.println(list1.isPalindrome());
 
 
 
+
+
+//        list1.printList();
+//        list1.reverseList();
+//        list1.printList();
+
+      //  System.out.println(list1.containsCycle());
+
+
+
+//        list1.head = createNodeWithLoop();
+//        System.out.println(list1.containsCycle());
+//
+//        Node<Integer> startOfCycle = list1.findStartOfCycle();
+//        System.out.println();
+
+
+
+//        Node<Integer> node = createNode();
+//        node = list1.reverseList(node);
+//        list1.printList(node);
 
     }
+
+
+    public static Node<Integer> mergeTwoSortedList(Node<Integer> node1, Node<Integer> node2){
+        if(node1 == null){
+            return node2;
+        }
+        if(node2 == null){
+            return node1;
+        }
+        Node<Integer> result = null;
+        if(node1.data < node2.data){
+            result = node1;
+            result.next = mergeTwoSortedList(node1.next, node2);
+        }else{
+            result = node2;
+            result.next = mergeTwoSortedList(node1, node2.next);
+        }
+        return result;
+    }
+
+    private static Node<Integer> createNode(){
+        Node<Integer> node = new Node<>(5);
+        node.next = new Node(4);
+        node.next.next = new Node(3);
+        node.next.next.next = new Node(2);
+        node.next.next.next.next = new Node(1);
+        return node;
+
+    }
+
+    private static Node<Integer> createNodeWithLoop(){
+        Node<Integer> node = new Node<>(1);
+        node.next = new Node(2);
+        node.next.next = new Node(3);
+        node.next.next.next = new Node(4);
+        node.next.next.next.next = new Node(5);
+        node.next.next.next.next.next = new Node(6);
+        node.next.next.next.next.next.next = new Node(7);
+        node.next.next.next.next.next.next.next = new Node(8);
+        node.next.next.next.next.next.next.next.next = new Node(9);
+
+        node.next.next.next.next.next.next.next.next.next = node.next.next; // 9 points to 3
+
+        return node;
+
+    }
+
+
 }
